449f11f28e5fa831ae2f854f966cda1715f622ea
Per Øyvind Karlsen <proyvind@moondrake.org>
2016-06-09 20:16:21 +0200

    indent

--- pwr_button/pwr_button.c
+++ pwr_button/pwr_button.c
@@ -37,72 +37,72 @@ module_param(debug, bool, S_IRUGO);
 MODULE_PARM_DESC(debug, "enable debug");
 
 #define pb_info(fmt, args...)  \
-    if (debug) printk(KERN_EMERG "[pwr_button]:" fmt,##args)
+	if (debug) printk(KERN_EMERG "[pwr_button]:" fmt,##args)
 #define pb_err(fmt, args...)  \
-    printk(KERN_ERR "[pwr_button] Err:" fmt,##args)
+	printk(KERN_ERR "[pwr_button] Err:" fmt,##args)
 #define pb_info_enter(fmt,...) \
-    pb_info("[pwr_button]: Enter func:%s\n", __func__);
+	pb_info("[pwr_button]: Enter func:%s\n", __func__);
 #define lmsw_info_leave(fmt,...) \
-    pb_info("[pwr_button]: Leave func:%s\n", __func__);
+	pb_info("[pwr_button]: Leave func:%s\n", __func__);
 
 struct pwr_button_dev {
-    struct input_dev *input;
-    struct timer_list timer;
-    struct work_struct check_work;
-    struct pwr_button_platform_data *pdata;
-    atomic_t checking;
-    int count;
+	struct input_dev *input;
+	struct timer_list timer;
+	struct work_struct check_work;
+	struct pwr_button_platform_data *pdata;
+	atomic_t checking;
+	int count;
 };
 
 static void pwr_button_do_work(struct work_struct *work)
 {
 	u8 val = 0;
-    struct pwr_button_dev *pwr_dev = container_of(work, struct pwr_button_dev, check_work);
-    
-    if (pwr_dev->pdata->get_status()) {
-        pwr_dev->count++; 
-        if (pwr_dev->count >= 2) 
-        { 
-            val = 1;        
-            pwr_dev->count = 0;
-            del_timer(&pwr_dev->timer);
-            atomic_set(&pwr_dev->checking, 1);
-        } else {
-            val = 0;
-            mod_timer(&pwr_dev->timer, jiffies + HZ);
-        }
-    } else {
-        val = 0;
-        pwr_dev->count = 0;
-        del_timer(&pwr_dev->timer);
-        atomic_set(&pwr_dev->checking, 1);
-    }
-    
-    input_report_key(pwr_dev->input, KEY_POWER, val);
-    input_sync(pwr_dev->input);
+	struct pwr_button_dev *pwr_dev = container_of(work, struct pwr_button_dev, check_work);
+
+	if (pwr_dev->pdata->get_status()) {
+		pwr_dev->count++;
+		if (pwr_dev->count >= 2)
+		{
+			val = 1;
+			pwr_dev->count = 0;
+			del_timer(&pwr_dev->timer);
+			atomic_set(&pwr_dev->checking, 1);
+		} else {
+			val = 0;
+			mod_timer(&pwr_dev->timer, jiffies + HZ);
+		}
+	} else {
+		val = 0;
+		pwr_dev->count = 0;
+		del_timer(&pwr_dev->timer);
+		atomic_set(&pwr_dev->checking, 1);
+	}
+
+	input_report_key(pwr_dev->input, KEY_POWER, val);
+	input_sync(pwr_dev->input);
 }
 
 static void pwr_button_do_timer(unsigned long arg)
 {
-    struct pwr_button_dev *pwr_dev = (struct pwr_button_dev *)arg;
-    schedule_work(&pwr_dev->check_work);
+	struct pwr_button_dev *pwr_dev = (struct pwr_button_dev *)arg;
+	schedule_work(&pwr_dev->check_work);
 }
 
 static irqreturn_t pwr_button_irq(int irq, void *pwr)
 {
-    struct pwr_button_dev *pwr_dev = (struct pwr_button_dev *)pwr;
+	struct pwr_button_dev *pwr_dev = (struct pwr_button_dev *)pwr;
 
-    pb_info("[pwr_button]: irq\n"); 
+	pb_info("[pwr_button]: irq\n"); 
 
-    if (atomic_dec_and_test(&pwr_dev->checking)) {
-        init_timer(&pwr_dev->timer);
-        pwr_dev->timer.function = pwr_button_do_timer;
-        pwr_dev->timer.data = (unsigned long)pwr_dev;
-        pwr_dev->timer.expires = jiffies + HZ;
-        add_timer(&pwr_dev->timer);
-        
-        INIT_WORK(&pwr_dev->check_work, pwr_button_do_work);
-    }
+	if (atomic_dec_and_test(&pwr_dev->checking)) {
+		init_timer(&pwr_dev->timer);
+		pwr_dev->timer.function = pwr_button_do_timer;
+		pwr_dev->timer.data = (unsigned long)pwr_dev;
+		pwr_dev->timer.expires = jiffies + HZ;
+		add_timer(&pwr_dev->timer);
+
+		INIT_WORK(&pwr_dev->check_work, pwr_button_do_work);
+	}
 
 	return IRQ_HANDLED;
 }
@@ -111,35 +111,35 @@ static int __init pwr_button_probe(struct platform_device *pdev)
 {
 	int err;
 	struct input_dev *input;
-    struct pwr_button_dev *pwr_dev; 
-    struct pwr_button_platform_data *pdata;
-    int irq; 
+	struct pwr_button_dev *pwr_dev; 
+	struct pwr_button_platform_data *pdata;
+	int irq; 
 
-    pdata = pdev->dev.platform_data;
-    if (!pdata) {
-        dev_err(&pdev->dev, "platform data is required!\n");
-        return -EINVAL;
-    }
-    
-    if (!pdata->init || !pdata->exit || !pdata->get_status) {
-        dev_err(&pdev->dev, "no platform-specific callbacks provided\n");
-        return -EINVAL;
-    }
-    irq = pdata->irq;
+	pdata = pdev->dev.platform_data;
+	if (!pdata) {
+		dev_err(&pdev->dev, "platform data is required!\n");
+		return -EINVAL;
+	}
 
-    pwr_dev = kzalloc(sizeof(struct pwr_button_dev), GFP_KERNEL);
-    if (!pwr_dev) {
-        return -ENOMEM;
-    }
-    pwr_dev->pdata = pdata;
+	if (!pdata->init || !pdata->exit || !pdata->get_status) {
+		dev_err(&pdev->dev, "no platform-specific callbacks provided\n");
+		return -EINVAL;
+	}
+	irq = pdata->irq;
 
-    err = pdata->init();
-    if (err < 0) { 
-        dev_err(&pdev->dev, "failed to initialize IRQ\n");
-        return -ENXIO;
-    }
+	pwr_dev = kzalloc(sizeof(struct pwr_button_dev), GFP_KERNEL);
+	if (!pwr_dev) {
+		return -ENOMEM;
+	}
+	pwr_dev->pdata = pdata;
 
-    atomic_set(&pwr_dev->checking, 1);
+	err = pdata->init();
+	if (err < 0) {
+		dev_err(&pdev->dev, "failed to initialize IRQ\n");
+		return -ENXIO;
+	}
+
+	atomic_set(&pwr_dev->checking, 1);
 
 	input = input_allocate_device();
 	if (!input) {
@@ -152,24 +152,24 @@ static int __init pwr_button_probe(struct platform_device *pdev)
 
 	input->name = "pwr_button";
 	input->phys = "pwr_button/input3";
-    input->id.bustype = BUS_HOST;
+	input->id.bustype = BUS_HOST;
 	input->dev.parent = &pdev->dev;
-    
+
 	err = request_threaded_irq(irq, NULL, pwr_button_irq,
-			                   IRQF_TRIGGER_RISING & IRQF_TRIGGER_MASK,
-			                   "pwr_button", pwr_dev);
+			IRQF_TRIGGER_RISING & IRQF_TRIGGER_MASK,
+			"pwr_button", pwr_dev);
 	if (err < 0) {
 		pb_err("Can't get IRQ for pwr_button: %d\n", err);
 		goto free_input_dev;
 	}
-    
+
 	err = input_register_device(input);
 	if (err) {
 		pb_err("Can't register power button: %d\n", err);
 		goto free_irq;
 	}
 
-    pwr_dev->input = input;
+	pwr_dev->input = input;
 	platform_set_drvdata(pdev, pwr_dev);
 
 	return 0;
@@ -183,19 +183,19 @@ free_input_dev:
 
 static int __exit pwr_button_remove(struct platform_device *pdev)
 {
-    struct pwr_button_dev *pwr_dev = platform_get_drvdata(pdev);
+	struct pwr_button_dev *pwr_dev = platform_get_drvdata(pdev);
 	struct input_dev *input = pwr_dev->input;
-    int irq = pwr_dev->pdata->irq;
-    
-    if (pwr_dev) {
-        if (pwr_dev->pdata->exit) {
-            pwr_dev->pdata->exit();
-        }
+	int irq = pwr_dev->pdata->irq;
 
-	    free_irq(irq, pwr_dev);
-	    input_unregister_device(input);
-        kfree(pwr_dev);
-    } 
+	if (pwr_dev) {
+		if (pwr_dev->pdata->exit) {
+			pwr_dev->pdata->exit();
+		}
+
+		free_irq(irq, pwr_dev);
+		input_unregister_device(input);
+		kfree(pwr_dev);
+	}
 
 	return 0;
 }
@@ -211,7 +211,7 @@ static struct platform_driver pwr_button_driver = {
 static int __init pwr_button_init(void)
 {
 	return platform_driver_probe(&pwr_button_driver,
-			                      pwr_button_probe);
+			pwr_button_probe);
 }
 module_init(pwr_button_init);
 
